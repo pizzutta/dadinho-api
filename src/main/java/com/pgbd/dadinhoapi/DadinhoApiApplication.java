48f7acde5fd12958cf6f304fc628397243855f06
Vitória Pizzutti <vitoria.pizzutti@bridge.ufsc.br>
2023-11-13 21:40:12 -0300

    first commit

---
+++ src/main/java/com/pgbd/dadinhoapi/DadinhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.pgbd.dadinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadinhoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadinhoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pgbd/dadinhoapi/DadinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgbd.dadinhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadinhoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
