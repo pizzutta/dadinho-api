8c8b86933baf0f42ccae38d89b87dcc6cbbfd9ac
Vitória Pizzutti <vitoria.pizzutti@bridge.ufsc.br>
2023-11-17 18:07:11 -0300

    fix: cors issue - adding headers

--- src/main/java/com/pgbd/dadinhoapi/security/SecurityFilter.java
+++ src/main/java/com/pgbd/dadinhoapi/security/SecurityFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,7 +32,14 @@ public class SecurityFilter extends OncePerRequestFilter {
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpStatus.OK.value());
+        }
+
         filterChain.doFilter(request, response);
     }
 
