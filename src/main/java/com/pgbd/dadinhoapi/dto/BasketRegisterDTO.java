c5236ae714e57e9100f1e7fc818bb70647b1b00b
Vitória Pizzutti <pizzutti.v@gmail.com>
2025-06-04 16:08:01 -0300

    feat: adding name field to basket

--- src/main/java/com/pgbd/dadinhoapi/dto/BasketRegisterDTO.java
+++ src/main/java/com/pgbd/dadinhoapi/dto/BasketRegisterDTO.java
@@ -1,11 +1,14 @@
 package com.pgbd.dadinhoapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record BasketRegisterDTO(
+        @NotBlank
+        String name,
         @NotNull
         Long levelId,
         @NotEmpty
