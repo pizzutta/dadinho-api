fcdbdc251f505ab705a773384a4799ee0063f04a
Vitória Pizzutti <pizzutti.v@gmail.com>
2025-06-05 01:42:56 -0300

    feat: saving metrics

--- src/main/java/com/pgbd/dadinhoapi/service/GameService.java
+++ src/main/java/com/pgbd/dadinhoapi/service/GameService.java
@@ -4,6 +4,12 @@ import com.pgbd.dadinhoapi.dto.UserAnswerDTO;
 import com.pgbd.dadinhoapi.game.model.Result;
 import com.pgbd.dadinhoapi.model.Item;
 import com.pgbd.dadinhoapi.model.Level;
+import com.pgbd.dadinhoapi.model.User;
+import com.pgbd.dadinhoapi.model.UserLevelMetrics;
+import com.pgbd.dadinhoapi.repository.ItemRepository;
+import com.pgbd.dadinhoapi.repository.LevelRepository;
+import com.pgbd.dadinhoapi.repository.UserLevelMetricsRepository;
+import com.pgbd.dadinhoapi.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,14 +22,41 @@ import static com.pgbd.dadinhoapi.game.GameCommandValidator.validate;
 public class GameService {
 
     @Autowired
-    private LevelService levelService;
+    private LevelRepository levelRepository;
     @Autowired
-    private ItemService itemService;
+    private ItemRepository itemRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserLevelMetricsRepository userLevelMetricsRepository;
 
     public Result submit(UserAnswerDTO data) {
-        Level level = levelService.findById(data.levelId()).orElseThrow(EntityNotFoundException::new);
-        List<Item> allItems = itemService.findAll();
-        return validate(data.userAnswers(), level, allItems);
+        Level level = levelRepository.findById(data.levelId()).orElseThrow(EntityNotFoundException::new);
+        User user = userRepository.findById(data.userId()).orElseThrow(EntityNotFoundException::new);
+        List<Item> allItems = itemRepository.findAll();
+
+        Result result = validate(data.userAnswers(), level, allItems);
+        saveMetrics(user, level, result, data.totalTime());
+
+        return result;
+    }
+
+    private void saveMetrics(User user, Level level, Result result, Integer totalTime) {
+        UserLevelMetrics metrics =
+                userLevelMetricsRepository.findByUser_IdAndLevel_Id(user.getId(), level.getId()).orElseGet(UserLevelMetrics::new);
+
+        metrics.setUser(user);
+        metrics.setLevel(level);
+
+        if (result.isValid()) {
+            metrics.setConcluded(true);
+            metrics.setTotalTime(totalTime);
+        } else {
+            metrics.setConcluded(false);
+            metrics.setAttempts(metrics.getAttempts() != null ? metrics.getAttempts() + 1 : 1);
+        }
+
+        userLevelMetricsRepository.save(metrics);
     }
 
 }
